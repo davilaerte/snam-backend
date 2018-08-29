c7b65ced3dfad3b5c47811cf2a9b0c0788f83fa1
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-08-29 17:39:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send(JSON.stringify({ users: [{ nome: 'Davi', senha: '1234' }, { nome: 'Lucas', senha: '1334' }] })))
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.json({ users: [{ nome: 'Davi', senha: '1234' }, { nome: 'Lucas', senha: '1334' }] })
+});
 
-app.get('/user', (req, res) => res.send(JSON.stringify({ nome: 'Davi', senha: '1234' })))
+app.get('/user', (req, res) => {
+  res.statusCode = 200;
+  res.json({ nome: 'Davi', senha: '1234' })
+});
 
-app.post('/user', (req, res) => res.send())
+app.post('/user', (req, res) => res.send('Got a POST request'))
 
-app.put('/user', (req, res) => res.send())
+app.put('/user', (req, res) => res.send('Got a PUT request'))
 
-app.listen(port, () => console.log('App listening on port ' + port + ' !'))
+app.delete('/user', (req, res) => res.send('Got a DELETE request'))
+
+app.listen(port, () => console.log('App listening on port ' + port + '!'))
