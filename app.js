7a508d9ead7e5f2fadeb17787032b72007078ea8
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-09-05 17:24:31 -0300

    Added tests for application

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8000;
+
+module.exports = app;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.json({ users: [{ nome: 'Davi', senha: '1234' }, { nome: 'Lucas', senha: '1334' }] })
+  res.status(200).json({ users: [{ nome: 'Davi', senha: '1234' }, { nome: 'Lucas', senha: '1334' }] });
 });
 
 app.get('/user', (req, res) => {
-  res.statusCode = 200;
-  res.json({ nome: 'Davi', senha: '1234' })
+  res.status(200).json({ nome: 'Davi', senha: '1234' });
 });
 
-app.post('/user', (req, res) => res.send('Got a POST request'))
+app.post('/user', (req, res) => {
+  let response = req.body;
+  response.received = true;
+  res.status(200).json(response);
+});
 
 app.put('/user', (req, res) => res.send('Got a PUT request'))
 
---
+++ app.test.js
@@ -0,0 +1,11 @@
+const app = require('./app.js');
+const request = require('supertest');
+
+request(app)
+  .get('/user')
+  .expect('Content-Type', /json/)
+  .expect('Content-Length', '30')
+  .expect(200)
+  .end(function (err, res) {
+    if (err) throw err;
+  });
