25881071972b12e19231224e0908bd0622f9052b
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-09-05 17:25:41 -0300

    Added test for endpoint user

--- app.test.js
+++ app.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 
 request(app)
   .get('/user')
-  .expect('Content-Type', /json/)
+  .expect('Content-Type', /json /)
   .expect('Content-Length', '30')
   .expect(200)
   .end(function (err, res) {
