3637cd56589b9cbfaa7bcbb1e543d6e4f3d700c8
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2018-10-08 19:30:11 -0300

    Add pageRepository

--- page/pageController.js
+++ page/pageController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Page = require('./Page');
+const pageRepository = require('./pageRepository');
 const notificationRepository = require('../notification/notificationRepository');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
@@ -12,54 +12,44 @@ router.post('/', async (req, res) => {
   try {
     req.body.idUserAdm = userId;
 
-    const page = await Page.create(req.body);
+    const page = pageRepository.create(req.body);
 
     await createPageNotification('Create', 'Created new page!', page.id, userId);
 
     return res.status(201).json(page);
   } catch (e) {
-    return res.status(400).json({ error: 'Failed ' + e });
+    return res.status(400).json({ error: 'Failed: ' + e });
   }
 });
 
 router.post('/:id/post', async (req, res) => {
-  const userId = req.userId;
-  const page = await Page.findById(req.params.id);
-
-  if (page.idUserAdm !== userId) {
-    return res.status(401).json({ error: 'User not allowed' });
-  }
-
   try {
-    page.posts.push(req.body);
-
-    const updatedPage = await page.save();
-
-    await createPageNotification('Create', 'Created new page post!', page.id, userId);
+    const updatedPage = await pageRepository.createPost(req.params.id, req.userId, req.body);
+    await createPageNotification('Create', 'Created new page post!', updatedPage.id, req.userId);
 
     return res.status(201).json(updatedPage);
   } catch (e) {
-    return res.status(400).json({ error: 'Failed ' + e });
+    return res.status(400).json({ error: 'Failed: ' + e });
   }
 });
 
 router.get('/', async (req, res) => {
   try {
-    const pages = await Page.find({});
+    const pages = await pageRepository.findAll();
 
     return res.status(200).json(pages);
   } catch (e) {
-    return res.status(400).json({ error: 'Failed ' + e });
+    return res.status(400).json({ error: 'Failed: ' + e });
   }
 });
 
 router.get('/:id', async (req, res) => {
   try {
-    const page = await Page.findById(req.params.id);
+    const page = await pageRepository.findById(req.params.id);
 
     return res.status(200).json(page);
   } catch (e) {
-    return res.status(400).json({ error: 'Failed ' + e });
+    return res.status(400).json({ error: 'Failed: ' + e });
   }
 });
 
---
+++ page/pageRepository.js
@@ -0,0 +1,29 @@
+const Page = require('./Page');
+
+const pageRepository = {};
+
+pageRepository.create = async (data) => {
+  const page = new Page(data);
+  return await page.save();
+};
+
+pageRepository.findById = async (id) => {
+  return await Page.findById(id);
+};
+
+pageRepository.findAll = async () => {
+  return await Page.find({});
+};
+
+pageRepository.createPost = async (idPage, idUser, data) => {
+  const page = await pageRepository.findById(idPage);
+
+  if (page.idUserAdm !== idUser) {
+    throw new Error('User not allowed');
+  }
+
+  page.posts.push(data);
+  return await page.save();
+};
+
+module.exports = pageRepository;
